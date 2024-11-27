bfaf224218cb99c6a0952c03d148295f8913480c
bidemi1986 <bidemi007@gmail.com>
2024-11-26 17:35:48 -0800

    logging

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
 
+print(f" AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_BUCKET_NAME => AWS_ACCESS_KEY: {AWS_ACCESS_KEY}, AWS_SECRET_KEY: {AWS_SECRET_KEY}, AWS_BUCKET_NAME: {AWS_BUCKET_NAME}")
 # Check if required credentials are available
 if not TMDB_API_KEY:
     st.error("TMDB API key not found. Please set up your API key in the .env file.")
